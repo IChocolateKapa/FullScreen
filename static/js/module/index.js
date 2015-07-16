70010fdbdc6788bc108239fb30cd6729edb27d25
echo <echocholatekapa@163.com>
2015-07-16 23:04:39 +0800

    angular js入门的小demo

---
+++ static/js/module/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2015/7/16.
+ */
+$(function () {
+
+})
+
+function HelloAngular($scope){
+    $scope.greeting = {text: 'hello'};
+    $scope.word = 'EchoBeauty';
+}
+
+function listCtl($scope){
+    $scope.names = ['echo', 'ldl', 'cyl'];
+}
