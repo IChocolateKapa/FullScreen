e70a8872853a311a7431074eab7bb708644f5e20
echo <echocholatekapa@163.com>
2015-07-17 19:43:18 +0800

    +angular js 小demo

---
+++ static/js/module/angulartest.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2015/7/16.
+ */
+function HelloAngular($scope){
+    $scope.greeting = {text: 'hello'};
+    $scope.word = 'EchoBeauty';
+}
+
+function listCtl($scope){
+    $scope.names = ['echo', 'ldl', 'cyl'];
+}
--- static/js/module/index.js
+++ static/js/module/index.js
@@ -1,15 +1,47 @@
 /**
  * Created by Administrator on 2015/7/16.
  */
-$(function () {
 
-})
+/*1，避免全局依赖
+* 2，避免第三方组件的破坏
+* 3，兼容jquery操作符 - $ */
+(function($){
 
-function HelloAngular($scope){
-    $scope.greeting = {text: 'hello'};
-    $scope.word = 'EchoBeauty';
-}
+    var defaults = {
+        'container' : '#container',//容器
+        'sections' : '.section',//子容器
+        'easing' : 'ease',//特效方式，ease-in,ease-out,linear
+        'duration' : 1000,//每次动画执行的时间
+        'pagination' : true,//是否显示分页
+        'loop' : false,//是否循环
+        'keyboard' : true,//是否支持键盘
+        'direction' : 'vertical',//滑动的方向 horizontal,vertical,
+        'onpageSwitch' : function(pagenum){}
+    };
 
-function listCtl($scope){
-    $scope.names = ['echo', 'ldl', 'cyl'];
+
+    $.fn.pageSwitdh = function(options){
+        var opts = $.extend({}, defaults, options || {});
+        return this.each(function(){
+
+        });
+    }
+})(jQuery)
+
+
+/*为jquery增加原型方法 例子*/
+$.fn.foo = function(){
+
+};
+/*这里$.fn === $.prototype*/
+
+
+
+/*Angular controller*/
+function imgList($scope){
+    $scope.imgs = new Array();
+    for(var i = 0; i < 13; i++){
+        $scope.imgs.push(i);
+    }
+    console.log($scope.imgs);
 }
