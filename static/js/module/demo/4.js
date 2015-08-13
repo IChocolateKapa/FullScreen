66f9443f63db26e9f8e7d6b56530a23b29c609b3
IChocolateKapa <echochocolatekapa@163.com>
2015-08-13 10:49:16 +0800

    +实现了图片预览放大的功能，下一步，封装一个组件，好开心！

--- static/js/module/demo/4.js
+++ static/js/module/demo/4.js
@@ -11,124 +11,56 @@ require.config({
 
 require(['jquery'], function($){
 
-    //autoSlide();
-
-
     $(function(){
-
-        $(".selDiv").mousemove(function(){
-            //console.log($(".selDiv").position().top)
-            //console.log($(".selDiv").position().left)
-            var posX = event.pageX;
-            var posY = event.pageY;
-            /*元素相对于浏览器的偏移*/
-            var eleOffTop = $(".selDiv").offset().top;
-            var eleOffLeft = $(".selDiv").offset().left;
-            /*获取元素的宽高*/
-            var eleW = $(".selDiv").width();
-            var eleH = $(".selDiv").height();
-            $(".selDiv").css({"left": posX-eleOffLeft-eleW/2 + "px", "top": posY-eleOffTop-eleH/2+"px"})
+        $(".ListItem").mousemove(function(){
+            moveWithMouse($(".ListItem"), $(".selDiv"));
+            /*获取元素覆盖下的图片的像素*/
+            var offX = $(".selDiv").position().left;
+            var offY = $(".selDiv").position().top;
+            $(".zoomDiv img").css({"top": "-" + offY*1.9 + "px", "left": "-" + offX*1.9 + "px"});
         })
 
-
-        var slideTime;
-        $(".slideTrigger li").mouseenter(function(){
-            clearInterval(globalSlide);
-            var $this = $(this);
-            var index = $this.index();
-            slideTime = setTimeout(function(){
-                $(".slideTrigger a").removeClass("curr");
-                $this.find("a").addClass("curr");
-                $(".ListItem").removeClass("curr");
-                $(".ListItem").eq(index).addClass("curr");
-            }, 800)
-        })
-
-        $(".slideTrigger a").mouseleave(function(){
-            clearTimeout(slideTime);
-            autoSlide();
-        })
-
-
-      $(".ListItem").each(function(index, ele){
-           $(ele).css({
-               "left": index*960 + "px"
-           })
-       })
-
-
-        $(".slider-next").click(function(){
-            clearInterval(globalSlide);
-            slideAnimate(true);
-            //autoSlide();
-        })
-        $(".slider-prev").click(function(){
-            clearInterval(globalSlide);
-            slideAnimate(false);
-            //autoSlide();
-        })
     })
 
 
 
-    var globalSlide;
-    function autoSlide(){
-        globalSlide = window.setInterval(function(){
-            slideAnimate(true);
-        }, 1000)
-    }
+    /*元素跟随鼠标移动*/
+    function moveWithMouse($parentObj, $obj){
+        /*获取鼠标相对于浏览器的偏移*/
+        var posX = event.pageX;
+        var posY = event.pageY;
 
-    function slideAnimate(flag){
+        /*父元素相对于浏览器的偏移*/
+        var eleOffLeft = $parentObj.offset().left;
+        var eleOffTop = $parentObj.offset().top;
 
-        var curIndex = $(".slideTrigger li.curr").index();
-        //var curIndex = $(".ListItem.curr").index();
+        /*获取元父元素的宽高*/
+        var eleW = $parentObj.width();
+        var eleH = $parentObj.height();
 
-        if(flag){
-            var nextIndex = curIndex + 1;
-            if(nextIndex > $(".ListItem").length - 1){
-                nextIndex = 0;
-            }
-        } else {
-            var nextIndex = curIndex - 1;
-            if(nextIndex < 0){
-                nextIndex = 5;
-            }
+        /*获取移动半透框（元素）的宽高*/
+        var jpW = $obj.width();
+        var jpH = $obj.height();
+
+        /*鼠标相对于父元素的偏移*/
+        var mouseElex = posX-eleOffLeft;
+        var mouseEleY = posY-eleOffTop;
+
+        $obj.css({"left": + mouseElex-jpW/2 + "px", "top": mouseEleY-jpH/2 + "px"});
+
+        if(mouseElex < jpW/2){
+            $obj.css({"left": + "0"})
+        } else if((eleW-mouseElex) < jpW/2){
+            $obj.css({"left": + eleW-jpW + "px"})
         }
-        console.log(curIndex)
 
-        //$(".ListItem").eq(curIndex).animate({"left": "-960px"}, 500);
-        //$(".ListItem").eq(curIndex).css({"left": "-960px"});
-
-        $(".List").css({"left": "-" + 960*nextIndex + "px"});
-
-        /*$(".ListItem").eq(curIndex).css({
-            "transform": "translate(-" + 960*nextIndex + "px)",
-            "-webkit-transform": "translate(-" + 960*nextIndex + "px)"
-        })*/
-
-        $(".ListItem").removeClass("curr");
-        $(".ListItem").eq(nextIndex).addClass("curr");
-       /* $(".ListItem").eq(nextIndex).css({
-            "transform": "translate(0)",
-            "-webkit-transform": "translate(0)"
-        })*/
-
-        $(".slideTrigger li").removeClass("curr");
-        $(".slideTrigger li").eq(nextIndex).addClass("curr");
-
-        //console.log($(".ListItem").eq(curIndex-1).css("left"))
-
-        //$(".ListItem").eq(curIndex).css({"left": 960*($(".ListItem").length-curIndex-1) + "px"});
-
-        //console.log($(".ListItem").eq(curIndex-1).css("left"))
-
-        //var cloCurEle = $(".ListItem").eq(curIndex).clone();
-        //$(".ListItem").eq(curIndex).remove();
-        //console.log(cloCurEle);
-        //cloCurEle.appendTo($(".List"))
-        //cloCurEle.insertAfter($(".ListItem").eq($(".ListItem").length - 1));
-
+        if(mouseEleY < jpH/2){
+            $obj.css({"top": + "0"})
+        } else if((eleH-mouseEleY) < jpH/2){
+            $obj.css({"top": + eleH-jpH + "px"})
+        }
 
     }
+
 })
 
